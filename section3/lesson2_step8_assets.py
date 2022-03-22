5e0cd019d7452fcdcb6390e54c8d7d8161e08e8b
vpsanderklo <sanderkloj@gmail.com>
2022-03-22 18:24:58 +0300

    commit on 'main'

---
+++ section3/lesson2_step8_assets.py
@@ -0,0 +1,12 @@
+assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_input_text(expected_result, actual_result):
+    assert expected_result == actual_result, \
+    f"expected {expected_result}, got {actual_result}"
+
+def test_substring(full_string, substring):
+    assert substring in full_string, \
+    f"expected {substring} to be substring of {full_string}"
+
+print("Let's count together: {}, then goes {}, and then {}".format("one", "two", "three"))
+test_substring('some_text', 'some')
